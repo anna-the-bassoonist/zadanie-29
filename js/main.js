fff50c3330b2be21ee1c66eafc009860287d4815
Anna Toeplitz <anafagocistka@gmail.com>
2018-07-11 19:39:51 +0200

    próby rozwiązania

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var liczbaPierwsza = 12;
+var liczbaDruga = 24;
+
+function add(liczbaPierwsza, liczbaDruga) {
+    var result = liczbaPierwsza + liczbaDruga;
+    return: result;
+   console.log(return);
+}
